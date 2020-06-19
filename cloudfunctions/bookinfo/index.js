ceb33513ed219f839d66029b0cdaad76105a00b2
pujx1075885060 <1075885060@qq.com>
2020-06-19 08:49:20 +0800

    提交rank相关内容

---
+++ cloudfunctions/bookinfo/index.js
@@ -0,0 +1,19 @@
+const cloud = require('wx-server-sdk')
+cloud.init({})
+const db = cloud.database()
+const _ = db.command
+
+exports.main = async (event, context) => {
+  try {
+    return await db.collection('mybook').where({
+      _openid: "oPhK65bGlyskyP1P1km3PSUkwm9l"
+    })
+      .update({
+        data: {
+          price: "100元"
+        },
+      })
+  } catch (e) {
+    console.error(e)
+  }
+}
--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -1,66 +1,43 @@
 // pages/detail/detail.js
+const request=require("../../utils/requests");
+var star = require("../../utils/star");
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+      id:""
   },
+  onLoad:function(options){
+      // 页面初始化 options为页面跳转所带来的参数
+      var that=this;
+      that.setData({ id:options.id});
+      wx.showToast({
+          title: '加载中',
+          icon: 'loading',
+          duration: 10000
+      })
+      request.getBookById(that.data.id,function(res){
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+          var types =res.data;
+          var rating = types.rating;
+          rating.block = star.get_star(rating.average);
 
+          res.data = types;
+          console.log(res.data);
+
+          that.setData({bookInfo:res.data});
+      });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onReady:function(){
+    // 页面渲染完成
+   wx.hideToast();
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onShow:function(){
+    // 页面显示
+    console.log("显示");
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onHide:function(){
+    // 页面隐藏
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onUnload:function(){
+    // 页面关闭
   }
 })
